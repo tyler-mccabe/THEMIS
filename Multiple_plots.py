70e80954db00b052e1f32116444613eb79561dd7
Tyler McCabe <tjmccab2@asu.edu>
2018-06-29 11:43:26 -0400

    Updated code to work for THB/THC on 07-14-2008. Need to fix for 08-19-2008.

--- Multiple_plots.py
+++ Multiple_plots.py
@@ -8,16 +8,24 @@ import datetime
 ## This code reads in a .tplot file and plots 6 parameters in 6 subplots
 ## Must change date, tplot, indicies of interest, save directory and plot Title
 
+
+'''
+PROGRESS: AT THIS POINT, THE AUGUST 19TH THEMIS FILE DOES NOT HAVE 
+DATA BREAKS SEPARATED BY NANS. NEED TO UPDATE CODE SO THAT IF 
+MIN_INDEX1/MAX_INDEX 1 IS EMPTY, THEN SEPARATE BASED ON A JUMP 
+FORWARD IN TIME. ie: if t[i+1] - t[i] > 100s
+'''
+
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 plt.rcParams['xtick.top'] = True
 plt.rcParams['ytick.right'] = True
 
 path = '/Volumes/Seagate/NASA/Summer2018/THEMIS/'
-date = '07_14_2008/'
-tplot = 'TPLOT_save_file_THC_new.tplot'
+date = '08_19_2008/'
+tplot = 'TPLOT_save_file_THC.tplot'
 savedir = 'Plots/THC/'
-plot_title = 'Themis C: 07-14-2008'
+plot_title = 'Themis C: ' + date[:-1]
 
 file = scipy.io.readsav(path+date+tplot,python_dict=True)
 dq = file['dq']
@@ -38,21 +46,20 @@ index6 = 90 ## Ion Temp Burst          (peib_avgtemp)
 ##############
 # Magnetic Field Magnitude Plot
 ##############
-t1 = dh[index1].x[0] - 1215993600
+t1 = dh[index1].x[0]# - 1215993600
 y1 = dh[index1].y[0]
 
 ##############
 # Magnetic Field Vectors
 ##############
-t2 = dh[index2].x[0] #- 1215993600
+t2 = dh[index2].x[0]# - 1215993600
 y2_x = dh[index2].y[0][0] #GSE Coordinates
 y2_y = dh[index2].y[0][1] #GSE Coordinates
 y2_z = dh[index2].y[0][2] #GSE Coordinates
-
 ##############
 # Electron Density
 ##############
-t3 = dh[index3].x[0] #- 1215993600
+t3 = dh[index3].x[0]# - 1215993600
 y3 = dh[index3].y[0]
 
 ##############
@@ -64,25 +71,14 @@ y4 = dh[index4].y[0]
 ##############
 # Ion Density
 ##############
-t5 = dh[index5].x[0] - 1215993600
+t5 = dh[index5].x[0]# - 1215993600
 y5 = dh[index5].y[0]
 
 ##############
 # Ion Temperature
 ##############
-t6 = dh[index6].x[0] #- 1215993600
+t6 = dh[index6].x[0]# - 1215993600
 y6 = dh[index6].y[0]
-
-##############
-# Interpolate Times
-##############        
-y1 = np.interp(t5,t1,y1)
-y2_x = np.interp(t5,t2,y2_x)
-y2_y = np.interp(t5,t2,y2_y)
-y2_z = np.interp(t5,t2,y2_z)
-y3 = np.interp(t5,t3,y3)
-y4 = np.interp(t5,t4,y4)
-y6 = np.interp(t5,t6,y6)
 
 ##############
 # Get slices to plot
@@ -96,6 +92,8 @@ for i in range(len(y1)-1):
         max_index1 = np.append(max_index1,i+1)
 if len(min_index1) > len(max_index1):
     max_index1 = np.append(max_index1,len(y1)-1)
+print('min 1 ', min_index1)
+print('max 1 ', max_index1)
 
 ##############
 # Overall Plot Structure
@@ -112,9 +110,19 @@ def unix_to_utc(unix_time_array):
     print("result type:",type(result))
     return result
 
+def get_begin_end_indicies(time_array,start_time,stop_time):
+    '''Get slice indicies for each of the variables to plot'''
+    time = np.asarray([])
+    for t in range(len(time_array)):
+        if time_array[t] >= start_time and time_array[t] <= stop_time:
+            time = np.append(time,time_array[t])
+    begin_index = list(time_array).index(np.nanmin(time))
+    end_index = list(time_array).index(np.nanmax(time))
+    return begin_index,end_index
+
 for i in range(6):
 
-    print(i)
+    print('Plot ',i+1)
 
     fig = plt.figure(figsize=(5.5,6.5))
     ax1 = fig.add_subplot(6,1,1)
@@ -124,26 +132,23 @@ for i in range(6):
     ax5 = fig.add_subplot(6,1,5)
     ax6 = fig.add_subplot(6,1,6)
     
-    start = int(min_index1[i]) + 1
-    xmin = t5[start] - 10 # 3.9
-    stop = int(max_index1[i]) - 1
-    xmax = t5[stop] + 10 # 3.9
-    
-    time = np.asarray([])
-    for t in range(len(t5)):
-        if t5[t] >= xmin and t5[t] <= xmax:  # Changed t1 --> t5
-            time = np.append(time,t5[t])
-    begin = list(t5).index(np.nanmin(time))  
-    end = list(t5).index(np.nanmax(time)) 
+    start = int(min_index1[i]) #+ 1
+    xmin = t1[start]# - 10 # 3.9
+    stop = int(max_index1[i]) #- 1
+    xmax = t1[stop]# + 10 # 3.9
 
-    ax1.plot(t5,y1,'k',lw=0.5)
+    begin, end = get_begin_end_indicies(t1, xmin, xmax)
+    ax1.plot(t1,y1,'k',lw=0.5)
     ax1.set_ylabel('$|\mathbf{B}_{o}| (nT)$')
     ax1.set_xlim(xmin,xmax)
     ax1.set_ylim(np.nanmin(y1[begin:end])*0.8, np.nanmax(y1[begin:end])*1.1)
+    ax1.set_xticklabels(unix_to_utc(ax1.get_xticks()))    
+    plt.setp(ax1.get_xticklabels(), visible=False) #Share x-axis
 
-    ax2.plot(t5,y2_x,'r',lw=0.5,label='X')
-    ax2.plot(t5,y2_y,'g',lw=0.5,label='Y')
-    ax2.plot(t5,y2_z,'b',lw=0.5, label ='Z')
+    begin, end = get_begin_end_indicies(t2, xmin, xmax)
+    ax2.plot(t2,y2_x,'r',lw=0.5,label='X')
+    ax2.plot(t2,y2_y,'g',lw=0.5,label='Y')
+    ax2.plot(t2,y2_z,'b',lw=0.5, label ='Z')
     # ax2.legend(loc='lower left', frameon=True,prop={'size': 6})
     ax2.set_ylabel('$\mathbf{B}_{o} (nT)$')
     ax2.set_xlim(xmin,xmax)
@@ -151,90 +156,65 @@ for i in range(6):
     maxs = np.asarray([np.nanmax(y2_x[begin:end]),np.nanmax(y2_y[begin:end]),np.nanmax(y2_z[begin:end])])
     min_y = np.nanmin(mins)
     max_y = np.nanmax(maxs) * 1.1
-    ax2.set_ylim(min_y, max_y)
-    
-    ax3.plot(t5,y3,'k',lw=0.5)
+    ax2.set_ylim(min_y, max_y) 
+    ax2.set_xticklabels(unix_to_utc(ax2.get_xticks()))    
+    plt.setp(ax2.get_xticklabels(), visible=False) #Share x-axis
+
+    begin, end = get_begin_end_indicies(t3, xmin, xmax)
+    ax3.plot(t3,y3,'k',lw=0.5)
     ax3.set_ylabel('$n_{e}\ (cm^{-3})$')
     ax3.set_xlim(xmin,xmax)
     ax3.set_ylim(np.nanmin(y3[begin:end])*0.8, np.nanmax(y3[begin:end])*1.1)
+    ax3.set_xticklabels(unix_to_utc(ax3.get_xticks()))    
+    plt.setp(ax3.get_xticklabels(), visible=False) #Share x-axis
 
+    begin, end = get_begin_end_indicies(t4, xmin, xmax)
     # ax4.semilogy(t5,y4,'k',lw=0.5)    
-    ax4.plot(t5,y4,'k',lw=0.5)
+    ax4.plot(t4,y4,'k',lw=0.5)
     ax4.set_ylabel('$T_{e}$ (eV)')
     ax4.set_xlim(xmin,xmax)
     ax4.set_ylim(np.nanmin(y4[begin:end])*0.8, np.nanmax(y4[begin:end])*1.1)
-    
+    diff = np.nanmax(y4[begin:end]) - np.nanmin(y4[begin:end])
+    if diff >= 1000:
+        ax4.cla()
+        ax4.semilogy(t4,y4,'k',lw=0.5)
+        ax4.set_ylabel('$T_{i}$ (eV)')
+        ax4.set_xlabel('Time From Start of Date (s)')
+        ax4.set_xlim(xmin,xmax)
+        ax4.set_ylim(np.nanmin(y4[begin:end])*0.8, np.nanmax(y4[begin:end])*1.1)
+    else:
+        pass
+    ax4.set_xticklabels(unix_to_utc(ax4.get_xticks()))    
+    plt.setp(ax4.get_xticklabels(), visible=False) #Share x-axis
+
+    begin, end = get_begin_end_indicies(t5, xmin, xmax)
     ax5.plot(t5,y5,'k',lw=0.5)
     ax5.set_ylabel('$n_{i}\ (cm^{-3})$')
     ax5.set_xlim(xmin,xmax)
     ax5.set_ylim(np.nanmin(y5[begin:end])*0.8, np.nanmax(y5[begin:end])*1.1)
-    # ax5.set_xticklabels(unix_to_utc(ax5.get_xticks()))
+    ax5.set_xticklabels(unix_to_utc(ax5.get_xticks()))
+    plt.setp(ax5.get_xticklabels(), visible=False) #Share x-axis
 
-    # ax6.semilogy(t5,y6,'k',lw=0.5)
-    ax6.plot(t5,y6,'k',lw=0.5)
+    begin, end = get_begin_end_indicies(t6, xmin, xmax)
+    ax6.plot(t6,y6,'k',lw=0.5)
     ax6.set_ylabel('$T_{i}$ (eV)')
     ax6.set_xlabel('Time From Start of Date (s)')
     ax6.set_xlim(xmin,xmax)
     ax6.set_ylim(np.nanmin(y6[begin:end])*0.8, np.nanmax(y6[begin:end])*1.1)
+    diff = np.nanmax(y6[begin:end]) - np.nanmin(y6[begin:end])
+    if diff >= 1000:
+        ax6.cla()
+        ax6.semilogy(t6,y6,'k',lw=0.5)
+        ax6.set_ylabel('$T_{i}$ (eV)')
+        ax6.set_xlabel('Time From Start of Date (s)')
+        ax6.set_xlim(xmin,xmax)
+        ax6.set_ylim(np.nanmin(y6[begin:end])*0.8, np.nanmax(y6[begin:end])*1.1)
+    else:
+        pass
     ax6.set_xticklabels(unix_to_utc(ax6.get_xticks()))
 
     fig.suptitle(plot_title)
-    fig.subplots_adjust(left=0.15, bottom=0.1, right=0.97, top=0.94, wspace=0.2, hspace=0.38)
+    fig.subplots_adjust(left=0.15, bottom=0.1, right=0.97, top=0.94, wspace=0.2, hspace=0.18) #hspace = 0.38 with labels
     plt.savefig(path + date + savedir + 'figure_%d.png'%(i+1))
+    # plt.show() 
     plt.clf()
-
-### Saving in case of need
-
-# print('min 1',min_index1)
-# print('max 1',max_index1)
-
-# min_index2 = np.asarray([])
-# max_index2 = np.asarray([])
-# for i in range(len(y2_x)-1):
-#     if m.isnan(y2_x[i]) == True and m.isnan(y2_x[i+1]) == False:
-#         min_index2 = np.append(min_index2,i)
-#     if m.isnan(y2_x[i]) == False and m.isnan(y2_x[i+1]) == True:
-#         max_index2 = np.append(max_index2,i+1)
-# print('min 2',min_index2)
-# print('max 2',max_index2)
-
-# min_index3 = np.asarray([])
-# max_index3 = np.asarray([])
-# for i in range(len(y3)-1):
-#     if m.isnan(y3[i]) == True and m.isnan(y3[i+1]) == False:
-#         min_index3 = np.append(min_index3,i)
-#     if m.isnan(y3[i]) == False and m.isnan(y3[i+1]) == True:
-#         max_index3 = np.append(max_index3,i+1)
-# print('min 3',min_index3)
-# print('max 3',max_index3)
-
-# min_index4 = np.asarray([])
-# max_index4 = np.asarray([])
-# for i in range(len(y4)-1):
-#     if m.isnan(y4[i]) == True and m.isnan(y4[i+1]) == False:
-#         min_index4 = np.append(min_index4,i)
-#     if m.isnan(y4[i]) == False and m.isnan(y4[i+1]) == True:
-#         max_index4 = np.append(max_index4,i+1)
-# print('min 4',min_index4)
-# print('max 4',max_index4)
-
-# min_index5 = np.asarray([])
-# max_index5 = np.asarray([])
-# for i in range(len(y5)-1):
-#     if m.isnan(y5[i]) == True and m.isnan(y5[i+1]) == False:
-#         min_index5 = np.append(min_index5,i)
-#     if m.isnan(y5[i]) == False and m.isnan(y5[i+1]) == True:
-#         max_index5 = np.append(max_index5,i+1)
-# print('min 5',min_index5)
-# print('max 5',max_index5)
-
-# min_index6 = np.asarray([])
-# max_index6 = np.asarray([])
-# for i in range(len(y6)-1):
-#     if m.isnan(y6[i]) == True and m.isnan(y6[i+1]) == False:
-#         min_index6 = np.append(min_index6,i)
-#     if m.isnan(y6[i]) == False and m.isnan(y6[i+1]) == True:
-#         max_index6 = np.append(max_index6,i+1)       
-# print('min 6',min_index6)
-# print('max 6',max_index6)
-
