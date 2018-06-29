634b2fed6b8d54dbcdc5c546b378374aeb1fd806
Tyler McCabe <tjmccab2@asu.edu>
2018-06-29 12:41:27 -0400

    Slight update.

---
+++ unix_to_utc.py
@@ -0,0 +1,13 @@
+def unix_to_utc(unix_time_array):
+    '''Takes array of tick labels in unix time
+    and converts them into readable utc
+    Make sure to import datetime'''
+    result = [None]*(len(unix_time_array))
+    for i in range(len(unix_time_array)):
+        result[i] = datetime.datetime.utcfromtimestamp(unix_time_array[i]
+        ).strftime('%H:%M:%S')
+    print("result type:",type(result))
+    return result
+
+ #EXAMPLE USAGE:
+ #ax6.set_xticklabels(unix_to_utc(ax6.get_xticks()));
